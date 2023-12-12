c660276d0abb16a43a46e97c2160990574e297a2
MagdaevDalambek <dalambekmagdaev11125@gmail.com>
2023-12-12 18:30:18 +0300

    update

--- prog/main.py
+++ prog/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import random as rnd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,38 +23,39 @@ def create_graph(x, y, aur, bur, namegraph):
     correlation_coefficient = np.corrcoef(y, x)[0, 1]
     return correlation_coefficient
 
-correlation_v = []
-# Цикл для создания двух графиков: один при среднем случае, второй при худшем
-for namegraph in ["Средний", "Худший"]:
-    x = [i for i in range(10, 10001, 10)]
-    time = []
-    randmax = 1000000
-    for i in x:
-        a = [rnd.randint(1, randmax) for j in range(i)]
-        if namegraph == "Средний":
-            b = a[rnd.randint(1, len(a)-1)]
-        else:
-            b = randmax+1
-        timer = (timeit.timeit(lambda: find(a, b, i), number=50))/50
-        time.append(timer)
+if __name__ == '__main__':
+    correlation_v = []
+    # Цикл для создания двух графиков: один при среднем случае, второй при худшем
+    for namegraph in ["Средний", "Худший"]:
+        x = [i for i in range(10, 10001, 10)]
+        time = []
+        randmax = 1000000
+        for i in x:
+            a = [rnd.randint(1, randmax) for j in range(i)]
+            if namegraph == "Средний":
+                b = a[rnd.randint(1, len(a)-1)]
+            else:
+                b = randmax+1
+            timer = (timeit.timeit(lambda: find(a, b, i), number=50))/50
+            time.append(timer)
 
-    # Вычисление коэффицентов в системе уравнений метода наименьших квадратов
-    sx = sum(x)
-    stime = sum(time)
-    sx2 = sum(i**2 for i in x)
-    sxtime = sum(i*j for i, j in zip(x, time))
-    n = len(x)
-    k = sx2/sx   # вычисление свободного коэффициента
-    bur = (sxtime - k*stime)/(sx-k*n)   # свободный коэффицент
-    aur = (stime - bur*n)/sx
+        # Вычисление коэффицентов в системе уравнений метода наименьших квадратов
+        sx = sum(x)
+        stime = sum(time)
+        sx2 = sum(i**2 for i in x)
+        sxtime = sum(i*j for i, j in zip(x, time))
+        n = len(x)
+        k = sx2/sx   # вычисление свободного коэффициента
+        bur = (sxtime - k*stime)/(sx-k*n)   # свободный коэффицент
+        aur = (stime - bur*n)/sx
 
-    # Создание графических окон
-    plt.figure(namegraph)
-    plt.subplots_adjust(left=0.2)
+        # Создание графических окон
+        plt.figure(namegraph)
+        plt.subplots_adjust(left=0.2)
 
-    # Создание графиков
-    correlation_v.append(create_graph(x, time, aur, bur, namegraph))
-print("Коэффициент корреляции в первом случае =", correlation_v[0], "\nКоэффициент корреляции во втором случае =", correlation_v[1])
+        # Создание графиков
+        correlation_v.append(create_graph(x, time, aur, bur, namegraph))
+    print("Коэффициент корреляции в первом случае =", correlation_v[0], "\nКоэффициент корреляции во втором случае =", correlation_v[1])
 
-# Показ графиков
-plt.show()
+    # Показ графиков
+    plt.show()
